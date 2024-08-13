6e5a2bbe81b6f586a45466150c356667cc6d612c
cameronaw13 <cameronawichman@gmail.com>
2024-08-12 17:38:43 -0700

    modularize bash, add custom rebuild script
    commit rebuilds on separated branches allowing for more extensive rollbacks (rebase squashes will be
    needed before merging into master)

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -1,7 +1,7 @@
 { lib, pkgs, inputs, ... }:
 {
   imports = [
-    ./upgrade-diff.nix
+    ./activation.nix
   ];
 
   /* Environment */
--- modules/users/programs/default.nix
+++ modules/users/programs/default.nix
@@ -5,6 +5,7 @@
     ./htop.nix
     ./git.nix
     ./ssh.nix
+    ./bash.nix
   ];
 
   options.local.homepkgs.hostname = lib.mkOption {
