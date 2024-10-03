4a6eafc029cdfa0743a584a767d29603ff148fc4
Cameron <113076756+cameronaw13@users.noreply.github.com>
2024-10-03 08:30:26 -0700

    Update nixos-anywhere.sh
    Fix indenting

--- nixos-anywhere.sh
+++ nixos-anywhere.sh
@@ -4,17 +4,17 @@
 # 1) https://manpages.ubuntu.com/manpages/xenial/en/man3/sd_booted.3.html
 
 user_continue() {
-	read -rp "Do you wish to continue? [y/N]; " choice
-	if [ "${choice,,}" != "y" ]; then
-		echo "Exiting..."
-		exit 2
-	fi
+  read -rp "Do you wish to continue? [y/N]; " choice
+  if [ "${choice,,}" != "y" ]; then
+    echo "Exiting..."
+    exit 2
+  fi
 }
 
-if [ "$UID' -lt 1000 ]; then
-	echo "Please do not run with a system account!"
-	echo "Exiting..."
-	exit 1
+if [ "$UID" -lt 1000 ]; then
+  echo "Please do not run with a system account!"
+  echo "Exiting..."
+  exit 1
 fi
 
 echo "Welcome to the nixos-anywhere script!"
@@ -32,16 +32,16 @@ user_continue
 while ! nix --version; do
   read -rp "It seems that nix isn't installed! Would you like to install nix? [y/N]: " nix_choice
   if [ "${nix_choice,,}" = "y" ]; then
-		# Install multi/single-user nix dependant on systemd init [1]
-		ls /run/systemd/system && nix_option="--daemon" || nix_option="--no-daemon"
-		echo "Installing nix in $nix_option mode"
-		sh <(curl -L https://nixos.org/nix/install) "$nix_option"
-		echo "Finished installing nix!"
-	else
-		echo "Suspend the process (^Z) and visit 'https://nixos.org/download/' to manually install nix"
-		echo "You can return with the 'fg' command, see builtins(1)"
-		echo
-		user_continue
+    # Install multi/single-user nix dependant on systemd init [1]
+    ls /run/systemd/system && nix_option="--daemon" || nix_option="--no-daemon"
+    echo "Installing nix in $nix_option mode"
+    sh <(curl -L https://nixos.org/nix/install) "$nix_option"
+    echo "Finished installing nix!"
+  else
+    echo "Suspend the process (^Z) and visit 'https://nixos.org/download/' to manually install nix"
+    echo "You can return with the 'fg' command, see builtins(1)"
+    echo
+    user_continue
   fi
 done
 
